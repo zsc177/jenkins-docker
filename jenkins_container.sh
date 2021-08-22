90e7bd91528e5d14c4d3b0b5dede4744ddc7c90d
Maxym Borachok <maxymborachok@gmail.com>
2021-08-22 22:26:52 +0300

    Update jenkins_container.sh

--- jenkins_container.sh
+++ jenkins_container.sh
@@ -1,1 +1,16 @@
 
+#!/bin/bash
+
+# run Jenkins container
+docker run \
+  --name instaprizer-jenkins \
+  --detach \
+  --publish 8080:8080 \
+  --publish 50000:50000 \
+  --volume /var/jenkins_home:/var/jenkins_home \
+  --volume /var/run/docker.sock:/var/run/docker.sock \
+  instaprizer-jenkins
+
+# show endpoint
+echo '##### Instaprizer Jenkins container successfully created #####'
+echo 'You can access Jenkins at: http://'$(curl -s ipconfig.co)':8080'
